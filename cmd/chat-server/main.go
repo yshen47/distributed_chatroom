6bf397bfdc5f23abe612106b72c94109c78f6bcd
yuanshen <yshen47@illinois.edu>
2019-02-12 20:33:36 -0600

    init commit

---
+++ cmd/chat-server/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) <= 2 {
+		fmt.Println("Enter server name")
+		return
+	}
+
+}
---
+++ models/message.go
@@ -0,0 +1,6 @@
+package models
+
+type Message struct {
+	content 	string
+	sender 		string
+}
